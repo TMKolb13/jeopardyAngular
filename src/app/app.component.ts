7234081cfe5f935d423bcdb23a762b3e512fa551
Teresa Kolb <teresa.kolb@libertymutual.com>
2017-09-18 14:05:17 -0700

    initial commit with service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { JeopardyService } from './jeopardy.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  questionInfo;
+
+  constructor(private jeopardyService: JeopardyService){}
+
+  //method called getDataFromService and inside it we call the getQuestionInfo method on the service itself in jeopardy.service.ts
+  getDataFromService(){
+    this.jeopardyService.getQuestionInfo()
+      .subscribe(
+        questionInfo => {
+          this.questionInfo = questionInfo[0]
+        }
+      )
+  }
+
+  ngOnInit(){
+    this.getDataFromService()
+  }
 }
